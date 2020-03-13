37e8c0e79317d7f502197b55ebc268e0964f38ec
nkoev <koev.niki@gmail.com>
2020-03-14 00:35:11 +0200

    "first database requests #14"

--- server/src/posts/posts.service.ts
+++ server/src/posts/posts.service.ts
@@ -1,4 +1,22 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm'
+import { Post } from '../entities/post.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class PostsService {}
+export class PostsService {
+
+  constructor(
+    @InjectRepository(Post) private readonly postRepository: Repository<Post>
+  ) { }
+
+  public async getAllPosts(): Promise<Post[]> {
+
+    return await this.postRepository.find({});
+  }
+
+  public async createPost(post: Partial<Post>): Promise<Post> {
+
+    return await this.postRepository.save(post)
+  }
+}
