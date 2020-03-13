f7859020783df72d639d10b1e735ddde90ffcb45
nkoev <koev.niki@gmail.com>
2020-03-14 00:35:11 +0200

    "first database requests #14"

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -4,10 +4,23 @@ import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { PostsModule } from './posts/posts.module';
 import { CommentsModule } from './comments/comments.module';
+import { TypeOrmModule } from '@nestjs/typeorm'
 
 @Module({
-  imports: [UsersModule, PostsModule, CommentsModule],
+  imports: [UsersModule, PostsModule, CommentsModule, TypeOrmModule,
+    TypeOrmModule.forRoot({
+      type: "mysql",
+      host: "localhost",
+      port: 3306,
+      username: "root",
+      password: "nepu6ete",
+      database: "forumdb",
+      entities: [
+        __dirname + "/**/*.entity{.js,.ts}"
+      ],
+      synchronize: true,
+    })],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
