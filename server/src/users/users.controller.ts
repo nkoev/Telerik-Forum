8ecbdd2e53cf0c71a3dee1e8207455561e35412e
0ligotann <tanya.mnv@gmail.com>
2020-03-14 15:13:03 +0200

    Connect usersService with new database #2

--- server/src/users/users.controller.ts
+++ server/src/users/users.controller.ts
@@ -3,27 +3,26 @@ import { UsersService } from './users.service'
 
 @Controller('users')
 export class UsersController {
+
     constructor(private readonly usersService: UsersService) { }
 
     // REGISTER
     @Post('')
     @HttpCode(HttpStatus.CREATED)
-    public registerUser(@Body() user: any) {
-        const { username, password } = user;
-
-        this.usersService.registerUser(username, password);
-
-        return { msg: 'User registered...'};
+    async registerUser(
+        @Body('username') username: string,
+        @Body('password') password: string
+    ) {
+        return await this.usersService.registerUser({"username": username, "password": password});
     }
 
-    // LOGIN
+    // // LOGIN
     @Post('/session')
     @HttpCode(HttpStatus.ACCEPTED)
-    public loginUser(@Body() user: any) {
-        const { username, password } = user;
-
-        this.usersService.loginUser(username, password);
-
-        return { msg: 'Success! Send JWT...'};
+    async loginUser(
+        @Body('username') username: string,
+        @Body('password') password: string
+    ) {
+        return await this.usersService.loginUser({"username": username, "password": password});
     }
 }
