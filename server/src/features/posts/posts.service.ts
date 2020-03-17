041733ffdf5688f9f405cd68a61c8a968f190f87
nkoev <koev.niki@gmail.com>
2020-03-17 20:31:52 +0200

    "types and DTOs added"

--- server/src/features/posts/posts.service.ts
+++ server/src/features/posts/posts.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm'
 import { Post } from '../../entities/post.entity';
 import { Repository } from 'typeorm';
+import { CreatePostDTO } from '../../models/posts/create-post.dto';
+import { PostDTO } from '../../models/posts/post.dto';
 
 @Injectable()
 export class PostsService {
@@ -10,13 +12,31 @@ export class PostsService {
     @InjectRepository(Post) private readonly postRepository: Repository<Post>
   ) { }
 
-  public async getAllPosts(): Promise<Post[]> {
+  public async getPosts(id: string): Promise<PostDTO[]> {
+    let posts = await this.postRepository.find({
+      where: { isDeleted: false }
+    });
 
-    return await this.postRepository.find({});
+    if (id) {
+      posts = posts.filter((post) => post.id === +id)
+    }
+
+    return posts.map((post) => this.PostEntityToDto(post));
   }
 
-  public async createPost(post: Partial<Post>): Promise<Post> {
+  public async createPost(post: CreatePostDTO): Promise<Post> {
 
-    return await this.postRepository.save(post)
+    const postEntity: Post = this.postRepository.create(post);
+
+    return await this.postRepository.save(postEntity)
+  }
+
+  private PostEntityToDto(entity: Post): PostDTO {
+    const dto = new PostDTO
+    dto.id = entity.id
+    dto.title = entity.title
+    dto.content = entity.content
+    dto.userId = entity.userId
+    return dto
   }
 }
