1c5b0a6d06dc6a6e4b6c3b76ff66c9a2beb3accd
nkoev <koev.niki@gmail.com>
2020-03-20 16:20:36 +0200

    "Update post functionality added #8"

--- server/src/features/posts/posts.controller.ts
+++ server/src/features/posts/posts.controller.ts
@@ -1,7 +1,8 @@
-import { Controller, Post, Body, Get, Param, HttpCode, HttpStatus } from '@nestjs/common';
+import { Controller, Post, Body, Get, Param, HttpCode, HttpStatus, Put } from '@nestjs/common';
 import { PostsService } from './posts.service';
 import { CreatePostDTO } from '../../models/posts/create-post.dto';
 import { PostDTO } from '../../models/posts/post.dto';
+import { UpdatePostDTO } from '../../models/posts/update-post.dto';
 
 @Controller('users/:userId/posts')
 export class PostsController {
@@ -17,7 +18,7 @@ export class PostsController {
 
   @Get('/:postId')
   @HttpCode(HttpStatus.OK)
-  async getSinglePosts(@Param('postId') postId: string): Promise<PostDTO> {
+  async getSinglePost(@Param('postId') postId: string): Promise<PostDTO> {
 
     return await this.postsService.getSinglePost(postId);
   }
@@ -30,6 +31,16 @@ export class PostsController {
   ): Promise<PostDTO> {
 
     return await this.postsService.createPost(post, userId);
+  }
+
+  @Put('/:postId')
+  @HttpCode(HttpStatus.ACCEPTED)
+  async updatePost(
+    @Param('postId') postId: string,
+    @Body() update: UpdatePostDTO
+  ): Promise<PostDTO> {
+
+    return await this.postsService.updatePost(update, postId);
   }
 
 }
