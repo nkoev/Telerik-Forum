c32c02fd2c6ab78c7054aa86a3726a8e72103347
nkoev <koev.niki@gmail.com>
2020-03-20 15:33:26 +0200

    "get posts endpoints and providers updated #5"

--- server/src/features/posts/posts.controller.ts
+++ server/src/features/posts/posts.controller.ts
@@ -1,28 +1,28 @@
-import { Controller, Post, Body, Get, Param, HttpCode, HttpStatus, Query } from '@nestjs/common';
+import { Controller, Post, Body, Get, Param, HttpCode, HttpStatus } from '@nestjs/common';
 import { PostsService } from './posts.service';
 import { CreatePostDTO } from '../../models/posts/create-post.dto';
 import { PostDTO } from '../../models/posts/post.dto';
 
-@Controller('users')
+@Controller('users/:userId/posts')
 export class PostsController {
 
   constructor(private readonly postsService: PostsService) { }
 
-  @Get('/:userId/posts')
+  @Get()
   @HttpCode(HttpStatus.OK)
-  async getUserPosts(@Param('userId') userId: string): Promise<PostDTO[]> {
+  async getPosts(): Promise<PostDTO[]> {
 
-    return await this.postsService.getUserPosts(userId);
+    return await this.postsService.getPosts();
   }
 
-  @Get('/posts')
+  @Get('/:postId')
   @HttpCode(HttpStatus.OK)
-  async getPosts(@Query('id') id: string): Promise<PostDTO[]> {
+  async getSinglePosts(@Param('postId') postId: string): Promise<PostDTO> {
 
-    return await this.postsService.getPosts(id);
+    return await this.postsService.getSinglePost(postId);
   }
 
-  @Post('/:userId/posts')
+  @Post()
   @HttpCode(HttpStatus.CREATED)
   async createPost(
     @Param('userId') userId: string,
@@ -31,4 +31,5 @@ export class PostsController {
 
     return await this.postsService.createPost(post, userId);
   }
+
 }
