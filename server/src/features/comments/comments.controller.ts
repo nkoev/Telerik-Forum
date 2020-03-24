2f6e42e8dd7fb0ed6575c2066f1f0aba536706ba
0ligotann <tanya.mnv@gmail.com>
2020-03-24 16:34:58 +0200

    Update create and read post comments #10 #11

--- server/src/features/comments/comments.controller.ts
+++ server/src/features/comments/comments.controller.ts
@@ -1,31 +1,30 @@
-import { Controller, Post, Body, Param, Get, HttpCode, HttpStatus } from '@nestjs/common';
+import { Controller, Post, Body, Param, Get, HttpCode, HttpStatus, Query } from '@nestjs/common';
 import { CommentsService } from './comments.service';
 import { CreateCommentDTO } from '../../models/comments/create-comment.dto';
 import { ShowCommentDTO } from '../../models/comments/show-comment.dto';
 
-@Controller('users')
+@Controller()
 export class CommentsController {
 
     constructor(private readonly commentsService: CommentsService) { }
 
-    @Get('/:userId/posts/:postId/comments')
+    @Get('/posts/:postId/comments')
     @HttpCode(HttpStatus.OK)
-    async readAllComments(
-        @Param('userId') userId: string,
+    async readPostComments(
         @Param('postId') postId: string
     ): Promise<ShowCommentDTO[]> {
 
-        return await this.commentsService.readAllComments(userId, postId);
+        return await this.commentsService.readPostComments(postId);
     }
 
-    @Post('/:userId/posts/:postId/comments')
+    @Post('/posts/:postId/comments')
     @HttpCode(HttpStatus.CREATED)
-    async createComment(
-        @Param('userId') userId: string,
+    async createPostComment(
+        @Query('userId') userId: string,
         @Param('postId') postId: string,
         @Body() comment: CreateCommentDTO
     ): Promise<ShowCommentDTO> {
 
-        return await this.commentsService.createComment(userId, postId, comment);
+        return await this.commentsService.createPostComment(userId, postId, comment);
     }
 }
