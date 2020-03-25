40f5842b50314e385367c70335b4f6e38466b8d2
0ligotann <tanya.mnv@gmail.com>
2020-03-26 01:42:11 +0200

    Add user validations

--- server/src/models/users/login-user.dto.ts
+++ server/src/models/users/login-user.dto.ts
@@ -1,6 +1,16 @@
+import { IsNotEmpty, IsString, MinLength, MaxLength } from "class-validator";
+
 export class LoginUserDTO {
 
+    @IsNotEmpty()
+    @IsString()
+    @MinLength(1)
+    @MaxLength(20)
     public username: string;
 
+    @IsNotEmpty()
+    @IsString()
+    @MinLength(1)
+    @MaxLength(20)
     public password: string;
 }
--- server/src/models/users/register-user.dto.ts
+++ server/src/models/users/register-user.dto.ts
@@ -1,6 +1,16 @@
+import { IsNotEmpty, IsString, MinLength, MaxLength } from "class-validator";
+
 export class RegisterUserDTO {
 
+    @IsNotEmpty()
+    @IsString()
+    @MinLength(1)
+    @MaxLength(20)
     public username: string;
 
+    @IsNotEmpty()
+    @IsString()
+    @MinLength(1)
+    @MaxLength(20)
     public password: string;
 }
