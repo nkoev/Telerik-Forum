cde3aa0b9e79192459fb7915a8b044c46f484352
0ligotann <tanya.mnv@gmail.com>
2020-03-31 18:48:28 +0300

    Update notifications when post was flagged #22

--- server/src/features/posts/posts.service.ts
+++ server/src/features/posts/posts.service.ts
@@ -115,6 +115,9 @@ export class PostsService {
       await postVotes
         .add(userId)
 
+
+    // await this.notificationsService.notifyUsers(userId, NotificationType.Post, ActionType.Like, `posts/${postId}`);
+
     return this.toPostDTO(post)
   }
 
@@ -148,8 +151,9 @@ export class PostsService {
       await postFlags
         .add(userId)
 
-    // Send notification to admin
-    await this.notificationsService.notify(NotificationType.Post, ActionType.Flag, foundPost.id);
+    // Send notification to admins
+    await this.notificationsService.notifyAdmins(NotificationType.Post, ActionType.Flag, `posts/${postId}`);
+    // await resourceService(Post).notify(NotificationType.Post, ActionType.Flag, foundPost.id);
 
     return this.toPostDTO(foundPost)
   }
--- server/src/models/notifications/show-notification.dto.ts
+++ server/src/models/notifications/show-notification.dto.ts
@@ -7,10 +7,12 @@ export class ShowNotificationDTO {
     public id: number;
     public type: NotificationType;
     public action: ActionType;
+    public target: string;
 
     constructor(notification: Notification) {
         this.id = notification.id;
         this.type = notification.type;
         this.action = notification.action;
+        this.target = notification.target;
     }
 }
