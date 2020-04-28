d209533425cd548bed584d880bcafe9a83d3aec3
nkoev <koev.niki@gmail.com>
2020-04-28 20:37:44 +0300

    "Register component updated #34"

--- client/src/app/modules/users/pages/register/register.component.ts
+++ client/src/app/modules/users/pages/register/register.component.ts
@@ -3,6 +3,8 @@ import { FormGroup, Validators, FormBuilder } from '@angular/forms';
 import { passwordsValidator } from 'src/app/modules/users/validators/match-passwords';
 import { Router } from '@angular/router';
 import { UsersDataService } from '../../services/users-data.service';
+import { map } from 'rxjs/operators';
+import { UserDTO } from 'src/app/models/user.dto';
 
 @Component({
   selector: 'app-register',
@@ -10,49 +12,80 @@ import { UsersDataService } from '../../services/users-data.service';
   styleUrls: ['./register.component.css'],
 })
 export class RegisterComponent implements OnInit {
-  registerForm: FormGroup;
-  invalid: boolean;
+  regForm: FormGroup;
+  registeredUsernames: string[];
+  errorMessage: string;
+  successMessage: string;
 
   constructor(
     private fb: FormBuilder,
     private router: Router,
-    private registerService: UsersDataService
+    private usersDataService: UsersDataService
   ) {}
 
   ngOnInit() {
-    this.registerForm = this.fb.group(
+    this.regForm = this.fb.group(
       {
-        username: [null, [Validators.required, Validators.minLength(5)]],
-        password: [null, [Validators.required, Validators.minLength(5)]],
-        confirmPassword: null,
+        username: [
+          null,
+          [
+            Validators.required,
+            Validators.minLength(5),
+            Validators.maxLength(20),
+          ],
+        ],
+        password: [
+          null,
+          [
+            Validators.required,
+            Validators.minLength(5),
+            Validators.maxLength(20),
+          ],
+        ],
+        confirmPassword: [null, [Validators.required]],
       },
       { validators: [passwordsValidator] }
     );
+    this.usersDataService
+      .getAllUsers()
+      .pipe(map((users: UserDTO[]) => users.map((user) => user.username)))
+      .subscribe((users: string[]) => (this.registeredUsernames = users));
   }
 
   get username() {
-    return this.registerForm.get('username');
+    return this.regForm.get('username');
   }
   get password() {
-    return this.registerForm.get('password');
+    return this.regForm.get('password');
   }
   get confirmPassword() {
-    return this.registerForm.get('confirmPassword');
+    return this.regForm.get('confirmPassword');
   }
 
   onSubmit(form: FormGroup) {
-    if (form.invalid) {
-      this.invalid = true;
-    } else {
-      this.registerService
-        .register(this.username.value, this.password.value)
-        .subscribe(
-          () => {
-            this.invalid = false;
-            setTimeout(() => this.router.navigateByUrl('login'), 3000);
-          },
-          (err) => console.log(err)
-        );
+    console.log(form);
+    const username = form.get('username').value;
+    const password = form.get('password').value;
+    this.successMessage = '';
+    this.errorMessage = '';
+
+    form.invalid
+      ? (this.errorMessage = 'Please check if all the fields are correct')
+      : this.registeredUsernames.includes(username)
+      ? (this.errorMessage = 'Username already taken')
+      : (this.successMessage = 'You have registered Successfully');
+
+    if (this.successMessage) {
+      this.registerUser(username, password);
     }
+  }
+
+  private registerUser(username: string, password: string) {
+    this.usersDataService.register(username, password).subscribe(
+      () => {
+        setTimeout(() => this.router.navigateByUrl('login'), 3000);
+      },
+      (err) => console.log(err)
+    );
   }
 }
